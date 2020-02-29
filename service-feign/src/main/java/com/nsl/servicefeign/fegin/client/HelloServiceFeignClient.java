485be7c1037cfebb38dc8b404f7704d351f7f8c8
shenglongNi <shenglongNi@163.com>
2020-02-29 22:22:34 +0800

    spring cloud feign

--- service-feign/src/main/java/com/nsl/servicefeign/fegin/client/HelloServiceFeignClient.java
+++ service-feign/src/main/java/com/nsl/servicefeign/fegin/client/HelloServiceFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "hello-service")
+@FeignClient(value = "hello-service", configuration = CustFeignConfiguration.class)
 public interface HelloServiceFeignClient {
 
 
