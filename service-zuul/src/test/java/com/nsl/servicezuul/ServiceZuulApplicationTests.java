76b3a8a813914c88cf0ac6faf2a65068aa44c2a6
shenglongNi <shenglongNi@163.com>
2020-03-09 17:54:34 +0800

    zuul config

---
+++ service-zuul/src/test/java/com/nsl/servicezuul/ServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsl.servicezuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
